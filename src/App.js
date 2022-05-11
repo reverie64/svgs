e40a2593426f2891a923f69694a0c6ab6375b9e4
reverie64 <kristinh889@gmail.com>
2022-05-10 23:43:24 -0500

    testing svg, add logo.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
+import { ReactComponent as HpBar } from './svgs/HpBar.svg';
+import { ReactComponent as MpBar } from './svgs/MpBar.svg';
+import { ReactComponent as Logo } from './svgs/Logo.svg';
 
 
 function App() {
+
+  var fills = document.querySelectorAll(".healthbar_fill");
+  
+  var health = 70;
+  var maxHp = 100;
+  
+  function renderHealth() {
+     
+     var percent = health / maxHp * 100;
+     
+     //Update color
+     document.documentElement.style.setProperty('--bar-fill', '#57e705');
+     document.documentElement.style.setProperty('--bar-top',  '#6aff03');
+     
+     if (percent <= 50) { //yellows
+        document.documentElement.style.setProperty('--bar-fill', '#d6ed20');
+        document.documentElement.style.setProperty('--bar-top',  '#d8ff48');   
+     }
+     if (percent <= 25) { //reds
+        document.documentElement.style.setProperty('--bar-fill', '#ec290a');
+        document.documentElement.style.setProperty('--bar-top',  '#ff3818');
+     }
+  
+  
+  
+  
+     fills.forEach(fill => {
+        fill.style.width = percent+"%";
+     })   
+  }
+  
+  function updateHealth(change) {
+     health += change;
+     health = health > maxHp ? maxHp : health;
+     health = health < 0 ? 0 : health;
+  
+     renderHealth();
+  }
+  
+  
+  //init
+  updateHealth(0)
+  
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <Logo />  
+      hp: <HpBar />
+    mp: <MpBar />
       </header>
     </div>
   );

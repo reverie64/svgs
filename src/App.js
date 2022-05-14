4c6fe72d368745c0880b261a6d664a8002fb2248
reverie64 <kristinh889@gmail.com>
2022-05-14 12:58:40 -0500

    added base svgs.

--- src/App.js
+++ src/App.js
@@ -5,50 +5,6 @@ import { ReactComponent as Logo } from './svgs/Logo.svg';
 
 function App() {
 
-  var fills = document.querySelectorAll(".healthbar_fill");
-  
-  var health = 70;
-  var maxHp = 100;
-  
-  function renderHealth() {
-     
-     var percent = health / maxHp * 100;
-     
-     //Update color
-     document.documentElement.style.setProperty('--bar-fill', '#57e705');
-     document.documentElement.style.setProperty('--bar-top',  '#6aff03');
-     
-     if (percent <= 50) { //yellows
-        document.documentElement.style.setProperty('--bar-fill', '#d6ed20');
-        document.documentElement.style.setProperty('--bar-top',  '#d8ff48');   
-     }
-     if (percent <= 25) { //reds
-        document.documentElement.style.setProperty('--bar-fill', '#ec290a');
-        document.documentElement.style.setProperty('--bar-top',  '#ff3818');
-     }
-  
-  
-  
-  
-     fills.forEach(fill => {
-        fill.style.width = percent+"%";
-     })   
-  }
-  
-  function updateHealth(change) {
-     health += change;
-     health = health > maxHp ? maxHp : health;
-     health = health < 0 ? 0 : health;
-  
-     renderHealth();
-  }
-  
-  
-  //init
-  updateHealth(0)
-  
-  
-
 
 
   return (
